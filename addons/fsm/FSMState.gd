2a565c2d07896e23f8bc962692ca31807eca2a22
Clearleaf <jake.rubia@gmail.com>
2025-12-31 23:43:50 -0400

    Add pop_state function to FSMState

--- addons/fsm/FSMState.gd
+++ addons/fsm/FSMState.gd
@@ -6,3 +6,5 @@ var actor : Node
 @abstract func _enter_state() -> void
 @abstract func _exit_state() -> void
 @abstract func _physics_process(delta: float) -> void
+
+func pop_state(): get_parent().pop_state()
