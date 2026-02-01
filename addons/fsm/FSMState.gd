0400fc8b93a7903d8a49b1f31f997266e5a51d37
Clearleaf <jake.rubia@gmail.com>
2026-02-01 12:31:58 -0400

    Remove pop_state function from FSMState.gd

--- addons/fsm/FSMState.gd
+++ addons/fsm/FSMState.gd
@@ -6,5 +6,3 @@ var actor : Node
 @abstract func _enter_state() -> void
 @abstract func _exit_state() -> void
 @abstract func _physics_process(delta: float) -> void
-
-func pop_state(): get_parent().pop_state()
