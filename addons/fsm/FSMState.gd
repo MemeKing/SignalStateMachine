fbf7033d6bb12e8d94539003c5d6ca37cf8ba884
Clearleaf <jake.rubia@gmail.com>
2026-04-30 13:44:21 -0300

    jhjk

--- addons/fsm/FSMState.gd
+++ addons/fsm/FSMState.gd
@@ -1,8 +1,27 @@
 @abstract
 class_name FSMState extends Node
+## Class for states used by the [SignalStateMachine] node.
+##
+## Extend this class to create a new state. All state behavior must be implemented
+## in physics_process. State nodes are turned "on" and "off" using their physics_process.
+## When physics_process is turned on, the state's physics_process will not run 
+## until the next physics frame, ensuring two states don't run simultaneously.
 
+
+## The node that this state is acting on. [br]
+## [code]actor = actor as Type[/code] will give you code completion in the script editor.
 var actor : Node
 
-@abstract func _enter_state() -> void
-@abstract func _exit_state() -> void
-@abstract func _physics_process(delta: float) -> void
+## Code to run when entering this state. Runs before physics_process. Use for 
+## setting up timers or whatever else. Optional.
+func _enter_state() -> void: pass
+
+## Code to run when exiting this state. Runs after physics_process. Optional.
+func _exit_state() -> void: pass
+
+
+func _physics_process(delta: float) -> void : pass
+
+
+## Reset the fsm back to it's default state. 
+func revert(): get_parent().revert_to_default_state()
--- addons/fsm/FiniteStateMachine.gd
+++ addons/fsm/FiniteStateMachine.gd
@@ -1,29 +1,65 @@
 @icon("res://addons/fsm/icon.png")
+class_name SignalStateMachine extends Node
+## General purpose state machine using nodes and signals.
+##
+## Instantiate as a child to whatever entity should be FSM controlled. FSMStates can then be
+## instantiated as children of the FSM. Linking of states should be done in the parent. 
+## [br][br]
+## Behavior coming from the FSM does not override the actor's own physics_process().
+## Any parent code will be "global" and run alongside whatever state code is running.
+## [br] [br]
+## See [FSMState] for more information on making states.
 
-class_name FiniteStateMachine extends Node
+## The scene/entity that this FSM is controlling. Can be any Node type, but take 
+## care in assigning states. If a state tries to access a non-existent property 
+## or function, a crash will occur.
+@export var actor : Node
 
-var state: FSMState
-var hist : Array
+## The state the fsm is currently in. Setting this before the FSM runs will set a default state.
+## Otherwise for reference only. Use [change_state()] or [revert()] to force a state change.
+var current_state: FSMState
 
-func _ready():
-	for i in get_children(): 
-		i.actor = get_parent()
-		i.set_physics_process(false)
-	call_deferred("deferred_start")
 
-func _change_state(new_state: FSMState):
-	if state is FSMState: # Will skip if current state is null.
-		state.set_physics_process(false) 
-		state._exit_state()
+## Link a state's signal to a target state. The FSM will then transition to the target state whenever
+## the signal is emitted. It's best to run this from the actor's _ready().[br][br]
+## [b]Example:   [/b][code]fsm.connect(airborne_state, "touched_wall", wallslide_state)[/code]
+func link(source: FSMState, exit_signal: String, target: FSMState):
+	source.connect(exit_signal, Callable(self, "change_state").bind(target))
+
+## Transition to the default state from anywhere. Using this can reduce flexibility when combining states. 
+func revert_to_default_state():
+	change_state(get_child(0))
+
+## Transition to the provided state. 
+func change_state(new_state: FSMState):
+	if current_state is FSMState:
+		current_state.set_physics_process(false)
+		current_state._exit_state()
+
 	if new_state is FSMState:
-		state = new_state
-		state.set_physics_process(true)
-		state._enter_state()
-		hist.insert(0,state)
-		hist.resize(5)
+		current_state = new_state
+		current_state.set_physics_process(true)
+		current_state._enter_state()
+	else:
+		push_error(get_parent().name + "'s fsm entered invalid state." )
 
-func link(source_state,signal_name,linked_state):
-	source_state.connect( signal_name, Callable(self, "_change_state").bind(linked_state) )
 
-func deferred_start():
-	_change_state(get_child(0))
+func _ready() -> void:
+	get_parent().ready.connect(setup)
+
+
+## Initialize state collection. Runs automatically when actor emits the ready 
+## signal. If you add a child state during gameplay you must run this immediately.
+func setup():
+	if not actor:
+		actor = get_parent()
+
+	for child_state in get_children():
+		# Add dependency injection here if needed.
+		child_state.actor = actor
+		child_state.set_physics_process(false)
+	
+	if current_state == null:
+		current_state = get_child(0)
+	
+	change_state(current_state)
